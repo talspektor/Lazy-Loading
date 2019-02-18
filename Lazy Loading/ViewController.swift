058550bfbb6624f1bf56063f4977e3f9142aa8b8
user140592 <user140592@eu201.local>
2019-02-18 19:27:30 +0000

    Initial Commit

---
+++ Lazy Loading/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lazy Loading
+//
+//  Created by user140592 on 2/18/19.
+//  Copyright © 2019 talspektor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
